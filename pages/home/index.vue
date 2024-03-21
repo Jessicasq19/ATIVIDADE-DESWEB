a09efeeaaafa5a8cea1f1a3a64a01b103001d971
Jessica <jessicasantanamss@gmail.com>
2024-03-21 20:27:00 -0300

    first commit

---
+++ pages/home/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+
+const logout = async()=>{ // async = assincrona
+        console.log("Successfully logged out!");
+        try{
+            //await = espere (espere o signOut)
+            await signOut({redirect: false});
+            navigateTo('/');
+        } catch(error){
+            //catch = se não (se não fizer o signOut, cai no catch)
+            console.log("Error when trying to execute logout", error);
+        }}
+
+definePageMeta({
+    middleware: 'auth'
+
+})
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1>    
+        <button @click="logout" class="customButton">SAIR</button>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
